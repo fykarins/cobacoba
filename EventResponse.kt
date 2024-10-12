cae4ed16fb8f3686ff99114af39843515f34b44d
Fay Ivana Karin <145007482+fykarins@users.noreply.github.com>
2024-10-12 21:12:28 +0700

    Create EventResponse.kt

---
+++ EventResponse.kt
@@ -0,0 +1,58 @@
+package com.example.dicodingevent.data.response
+
+import com.google.gson.annotations.SerializedName
+
+data class EventResponse(
+	@field:SerializedName("events") // Pastikan ini sesuai dengan nama di JSON
+	val events: List<ListEventsItem>,
+
+	@field:SerializedName("error")
+	val error: Boolean,
+
+	@field:SerializedName("message")
+	val message: String
+)
+
+data class ListEventsItem(
+	@field:SerializedName("summary")
+	val summary: String,
+
+	@field:SerializedName("mediaCover")
+	val mediaCover: String,
+
+	@field:SerializedName("registrants")
+	val registrants: Int,
+
+	@field:SerializedName("imageLogo")
+	val imageLogo: String,
+
+	@field:SerializedName("link")
+	val link: String,
+
+	@field:SerializedName("description")
+	val description: String,
+
+	@field:SerializedName("ownerName")
+	val ownerName: String,
+
+	@field:SerializedName("cityName")
+	val cityName: String,
+
+	@field:SerializedName("quota")
+	val quota: Int,
+
+	@field:SerializedName("name")
+	val name: String,
+
+	@field:SerializedName("id")
+	val id: Int,
+
+	@field:SerializedName("beginTime")
+	val beginTime: String,
+
+	@field:SerializedName("endTime")
+	val endTime: String,
+
+	@field:SerializedName("category")
+	val category: String
+)
