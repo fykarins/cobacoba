602c3e49b6a1db4a06cad0550fb71cbfddc1cb91
Fay Ivana Karin <145007482+fykarins@users.noreply.github.com>
2024-10-25 00:32:12 +0700

    Update MainActivity.kt

--- MainActivity.kt
+++ MainActivity.kt
@@ -1,55 +1,72 @@
-package com.example.dicodingevent
+package com.example.dicodingevent.ui.main
 
 import android.os.Bundle
-import android.util.Log
-import android.widget.Toast
+import android.os.StrictMode
+import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
-import androidx.recyclerview.widget.LinearLayoutManager
-import com.example.dicodingevent.data.response.EventResponse
-import com.example.dicodingevent.data.response.ListEventsItem
-import com.example.dicodingevent.data.retrofit.ApiConfig
+import androidx.appcompat.widget.SearchView
+import androidx.fragment.app.Fragment
+import com.example.dicodingevent.R
 import com.example.dicodingevent.databinding.ActivityMainBinding
-import retrofit2.Call
-import retrofit2.Callback
-import retrofit2.Response
+import com.example.dicodingevent.ui.event.FinishedFragment
+import com.example.dicodingevent.ui.event.HomeFragment
+import com.example.dicodingevent.ui.event.UpcomingFragment
+import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class MainActivity : AppCompatActivity() {
+
     private lateinit var binding: ActivityMainBinding
-    private lateinit var adapter: EventAdapter
-    private var eventsList: List<ListEventsItem> = listOf()
+    private val mainViewModel: MainViewModel by viewModels()
+    private fun loadFragment(fragment: Fragment) {
+        val transaction = supportFragmentManager.beginTransaction()
+        transaction.replace(R.id.fragment_container, fragment)
+        transaction.commit()
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        // Inisialisasi RecyclerView
-        binding.rvReview.layoutManager = LinearLayoutManager(this)
-        adapter = EventAdapter(eventsList)
-        binding.rvReview.adapter = adapter
-
-        // Panggil API untuk mendapatkan data event
-        fetchEvents()
-    }
-
-    private fun fetchEvents() {
-        val client = ApiConfig.getApiService().getEvents(0) // Mengambil data dengan query active=0
-        binding.progressBar.visibility = android.view.View.VISIBLE // Tampilkan progress bar
-        client.enqueue(object : Callback<EventResponse> {
-            override fun onResponse(call: Call<EventResponse>, response: Response<EventResponse>) {
-                binding.progressBar.visibility = android.view.View.GONE // Sembunyikan progress bar
-                if (response.isSuccessful) {
-                    val eventResponse = response.body()
-                    eventsList = eventResponse?.events ?: listOf()
-                    adapter.updateData(eventsList) // Panggil method updateData untuk memperbarui adapter
-                }
+        val bottomNavigation: BottomNavigationView = findViewById(R.id.nav_view)
+        bottomNavigation.setOnItemSelectedListener { item ->
+            val fragment = when (item.itemId) {
+                R.id.navigation_upcoming -> UpcomingFragment()
+                R.id.navigation_finished -> FinishedFragment()
+                R.id.navigation_home -> HomeFragment()
+                else -> null
             }
 
-            override fun onFailure(call: Call<EventResponse>, t: Throwable) {
-                binding.progressBar.visibility = android.view.View.GONE // Sembunyikan progress bar jika gagal
-                Log.e("MainActivity", "Error: ${t.message}")
-                // Contoh: Menampilkan Toast jika terjadi kegagalan
-                Toast.makeText(this@MainActivity, "Failed to load data", Toast.LENGTH_SHORT).show()
+            fragment?.let {
+                loadFragment(it)
+                true
+            } ?: false
+        }
+
+        if (savedInstanceState == null) {
+            loadFragment(HomeFragment())
+        }
+
+        StrictMode.setThreadPolicy(
+            StrictMode.ThreadPolicy.Builder()
+                .detectAll()
+                .penaltyLog()
+                .build()
+        )
+
+        setSupportActionBar(binding.toolbar)
+
+        binding.searchView.setOnQueryTextListener(object : SearchView.OnQueryTextListener {
+            override fun onQueryTextSubmit(query: String?): Boolean {
+                query?.let {
+                    mainViewModel.searchEvents(it)
+                }
+                return true
+            }
+
+            override fun onQueryTextChange(newText: String?): Boolean {
+                return false
             }
         })
     }
